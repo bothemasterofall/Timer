e1c6a7961a3eb8cfef340b6152c922b50275fa0e
Bo Cimino <bothemasterofall@gmail.com>
2016-07-31 17:18:41 -0500

    Added documentation

--- Timer.h
+++ Timer.h
@@ -3,6 +3,20 @@
 #include <vector>
 #include <ctime>
 
+/*******************************************************************************
+ * The Timer class is an object to assist with keeping time used by a program.
+ *
+ * setPoint sets a new time point.
+ *
+ * getClocks returns the number of clock ticks between the first and last points
+ * Its overloaded version may return the difference between two specified points
+ *
+ * getSeconds works similarly, but returns the difference in seconds.
+ *
+ * All methods return 0 if unsuccessful. This will happen if no points are set
+ * or if out of range intervals are specified. This will also happen if the
+ * first interval is greater than the second interval.
+*******************************************************************************/
 class Timer
 {
     public:
