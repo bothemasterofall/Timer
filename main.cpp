5b5afcf08c097df7711ff78b71914a997e83bb4f
Bo Cimino <bothemasterofall@gmail.com>
2016-07-30 21:08:30 -0500

    Added substance to main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,32 @@
 #include "Timer.h"
+#include <cstdio>
 
 int main()
 {
+    Timer timer;
+    timer.setPoint();
+
+    for (int i = 0; i < 10000; i++)
+    {
+        // Do stuff
+    }
+
+    timer.setPoint();
+
+    for (int i = 0; i < 20000; i++)
+    {
+        // Do other stuff
+    }
+
+    timer.setPoint();
+
+    printf("Total time in clocks: %lf\n", timer.getClocks());
+    printf("Total time in seconds: %lf\n", timer.getSeconds());
+
+    printf("Time between first two points in clocks: %lf\n",
+        timer.getClocks(0, 1));
+    printf("Time between first two points in seconds: %lf\n",
+        timer.getSeconds(0, 1));
+
     return 0;
 }
